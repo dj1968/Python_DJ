f263614f37382d5f01bedcd83bae592ecc5059dd
dj1968 <32321126+dj1968@users.noreply.github.com>
2022-01-04 10:13:44 +0100

    dj
    dj

---
+++ Test1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 18:24:37 2021
+
+@author: joerg
+"""
+
+a = 5 + 6
+
+a = "s33"
+b = "r555"
+
+c = a +b
+print (c + str(4) )
+
+var1 = "otto"
+var2 = "willi"
+var3 = "ziegenbbock"
+
+flutschies = ["ass", "fdsad", "sdfd"]
+print(flutschies)
+
+flutschies.append("sack")
+
+print(len (flutschies))
+
+
--- untitled14.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 15 18:24:37 2021
-
-@author: joerg
-"""
-
